7278789b5a3ff0dd7f3749c4587f7cce3be0bb41
Fernando Zhapa <ferynando7@gmail.com>
2018-04-30 00:26:19 -0500

    Initial commit

---
+++ Data/DataWork/Job.php
@@ -0,0 +1,65 @@
+<?php
+require_once '/var/www/html/JumpWebService1/Config/conf.php';
+require_once Conf::getRootDir().'Data/Entity.php';
+class Job extends Entity{
+   
+    private static $id;
+     
+    private static $idemployer;
+    private static $mode;
+    private static $state;
+    private static $idlocation;
+    private static $title;
+    private static $description;
+    private static $jobcost;
+    private static $dateposted;
+    private static $datestart;
+    private static $dateend;
+    private static $datepostend;
+    private static $availablepercentage;
+    private static $numberapplicants;
+    
+    static protected $tableName = 'job';
+    static protected $primaryKey = 'id';
+    
+    public function __construct($id=NULL, $idemployer=NULL, $mode=NULL, $state=NULL, $idlocation=NULL, $title=NULL, $description=NULL, $jobcost=NULL, $dateposted=NULL, $datestart=NULL, $dateend=NULL, $datepostend=NULL, $availablepercentage=NULL, $numberapplicants=NULL) {
+       
+        if (!is_null($id) && !is_null($idemployer) && !is_null($mode) && !is_null($state) && !is_null($idlocation) && !is_null($title) &&!is_null($description) &&!is_null($jobcost) &&!is_null($dateposted) &&!is_null($datestart) &&!is_null($dateend) &&!is_null($datepostend) &&!is_null($availablepercentage) &&!is_null($numberapplicants)){
+  
+        
+            $this->id = $id;
+            $this->idemployer = $idemployer;
+            $this->mode = $mode;
+            $this->state = $state;
+            $this->idlocation = $idlocation;
+            $this->title = $title;
+            $this->description = $description;
+            $this->jobcost = $jobcost;
+            $this->dateposted = $dateposted;
+            $this->datestart = $datestart;
+            $this->dateend = $dateend;
+            $this->datepostend = $datepostend;
+            $this->availablepercentage = $availablepercentage;
+            $this->numberapplicants = $numberapplicants;
+        
+        }
+    }
+
+    
+    //getter
+    public function get($attribut) {
+            if (property_exists($this, $attribut)) {
+                    return $this->$attribut;
+            }
+    }
+
+    //setter
+            public function set($attribut,$valeur) {
+                    if (property_exists($this, $attribut)) {
+                            $this->$attribut=$valeur;
+             }
+    }   
+
+}
+
+    
---
+++ Data/Entity.php
@@ -0,0 +1,171 @@
+<?php
+
+require_once '/var/www/html/JumpWebService1/Config/conf.php';
+
+
+
+class Entity{
+	public static $pdo;
+
+	public static function Init(){
+		$hostname=Conf::getHostname();
+		$database_name=Conf::getDatabase();
+		$login=Conf::getLogin();
+		$password=Conf::getPassword();
+		try {
+			self::$pdo = new PDO("pgsql:dbname=$database_name;host=$hostname", $login, $password);
+			self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			//if (Conf::getDebug()) {
+			//echo $e->getMessage(); // affiche un message d'erreur
+			//} else {
+			//echo 'Cnnection error';
+		//	}
+			die();
+		}
+	}
+	/*
+	public static function selectAll(){
+		$table_name=static::$tableName;
+		$class_name=ucfirst($table_name);
+		try {
+			$rep=Entity::$pdo->query("SELECT * FROM $table_name");
+		} catch (PDOException $e) {
+			if (Conf::getDebug()) {
+			echo $e->getMessage(); 
+			} else {
+			echo 'Connection error';
+			}
+			die();
+			}
+		$rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+		return $rep->fetchAll();
+	}
+*/
+        
+        
+        
+    public static function select($primary_value){
+        $table_name=static::$tableName;
+        $class_name=ucfirst($table_name);
+        $primary_key=static::$primaryKey;
+        $sql = "SELECT * from job WHERE id=1";
+        $values = array(
+                "primary_v" => $primary_value,
+        );
+        try{
+            
+            $req_prep=Entity::$pdo->query($sql);
+               
+        } catch (PDOException $e) {
+                if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+                } else {
+                       echo 'Une erreur est survenue <a href="./index.php"> retour a la page d\'accueil </a>';
+                }
+                die();
+        }
+        
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+        
+        
+        
+        $tab = $req_prep->fetch();
+        // Attention, si il n'y a pas de résultats, on renvoie false
+        //if (empty($tab))
+        //        return true;
+        return $tab;
+    }
+       
+	
+ /*
+	public static function delete($primary){
+		$table_name=static::$object;
+		$primary_key=static::$primary;
+		$sql= "DELETE FROM $table_name WHERE $primary_key=:primary_v";
+		$req_prep=Model::$pdo->prepare($sql);
+		$values = array(
+			"primary_v" => $primary,
+		);
+		try{
+			$req_prep->execute($values);
+		} catch (PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="./index.php"> retour a la page d\'accueil </a>';
+			}
+			return false;
+		}
+		return true;
+	}
+
+	public static function save($data){
+		$table_name=static::$object;
+		$sql= "INSERT INTO $table_name(";
+		foreach ($data as $cle => $valeur){
+			$sql .=" $cle,";
+		}
+		$sql=rtrim($sql,",").")";
+		$sql.=" VALUES (";
+		foreach ($data as $cle => $valeur){
+			$sql .=" :$cle,";
+		}
+		$sql=rtrim($sql,",").")";
+		$req_prep=Model::$pdo->prepare($sql);
+		try{
+			$req_prep->execute($data);
+		} catch (PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			}
+			return false;
+		}
+		return true;
+	}
+
+	public static function update($data){
+		$table_name=static::$object;
+		$primary_key=static::$primary;
+		$sql= "UPDATE $table_name SET";
+		foreach ($data as $cle => $valeur){
+			$sql .=" $cle=:$cle,";
+		}
+		$sql=rtrim($sql,",");
+		$sql.=" WHERE $primary_key=:$primary_key";
+		$req_prep=Model::$pdo->prepare($sql);
+		try{
+			$req_prep->execute($data);
+		} catch (PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="./index.php"> retour a la page d\'accueil </a>';
+			}
+			return false;
+		}
+		return true;
+	}
+
+	//getter
+	public function get($attribut) {
+		if (property_exists($this, $attribut)) {
+			return $this->$attribut;
+		}
+	}
+
+	//setter
+	public function set($attribut,$valeur) {
+		if (property_exists($this, $attribut)) {
+			$this->$attribut=$valeur;
+	 	}
+	}
+         * 
+         * 8
+         */
+
+}
+Entity::Init();
+?>
+
+
